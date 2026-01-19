5821f70db73e484d306808b4bd2854e12fefee15
henyanagar <henyanagar@gmail.com>
2026-01-19 23:39:53 +0200

    Updated logger logic and fixed DB connection

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // Server entry point - starts the Express application
 const app = require('./app');
+const mongoose = require('mongoose');
 const { pino } = require('./middlewares/logger');
 require('dotenv').config();
 
+
 const PORT = process.env.PORT || 3004;
+// Start server
+app.listen(PORT, () => {
+    console.log(`Admin service running on port ${PORT}`);
+    pino.info(`Admin service started on port ${PORT}`);
+});
